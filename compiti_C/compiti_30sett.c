bdad931cb1a3bf8d6da9fc529658cd57d8384bb9
testucoli <dario.mongardini@gmail.com>
2024-09-29 13:39:01 +0200

    spostato

---
+++ compiti_C/compiti_30sett.c
@@ -0,0 +1,11 @@
+/*Svolgere l'esercizio "Esempio: file di Record" sul file Gestione File in Moodle (ultime 2 slide): dopo aver caricato tutte le persone, implementare le seguenti 2 procedure/funzioni:
+- cercaPiuAnziano: è una procedura che, dati come parametri l'array di persone e il numero di persone, stampa a video i dati della persona più anziana
+- calcolaMaggiorenni: è una funzione che, dati come parametri l'array di persone e il numero di persone, restituisce il numero di persone maggiorenni.
+
+Nel main richiamare le suddette procedure/funzioni per testarle.
+
+POICHE' L'ORARIO E' STATO AGGIORNATO NELLA NOTTE FRA VENERDì E SABATO, CHI NON RIESCE A SVOLGERE QUESTI COMPITI O UNA PARTE DI ESSI, LI SVOLGERA' PER MARTEDì 1 OTTOBRE
+*/
+
+#include <stdio.h>
+
