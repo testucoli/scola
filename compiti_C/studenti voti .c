a095d99ee5a91391416d01823aa4b571c5f118ef
testucoli <dario.mongardini@gmail.com>
2024-10-09 18:52:43 +0200

    uppato

--- compiti_C/studenti voti .c
+++ compiti_C/studenti voti .c
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: silviasuriani
- *
- * Created on 3 ottobre 2024, 12.23
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,48 +17,57 @@ typedef struct {
 void insertstudente();
 void insertvoto();
 void stampastudenti();
+void calcolamedia();
 
 int main(int argc, char** argv) {
 
     int scelta;
 
     do {
-        printf("inserisci:\n\t1:inserire un nuovo studente\n\t2:inserire un nuovo voto\n\t3:stampa studenti\n\t0:per uscire");
+        printf("\nInserisci:\n\t1: Inserire un nuovo studente\n\t2: Inserire un nuovo voto\n\t3: Stampa studenti\n\t4: Calcola media voti e visualizza voti\n\t0: Per uscire\n");
         scanf("%d", &scelta);
         switch (scelta) {
-            case 1://inserimento studente
+            case 1:
                 insertstudente();
                 break;
-            case 2://inserimento voto
+            case 2:
                 insertvoto();
                 break;
-            case 3://stampa studenti
+            case 3:
                 stampastudenti();
                 break;
-            case 0://uscita
+            case 4:
+                calcolamedia();
+                break;
+            case 0:
                 break;
             default:
-                printf("\ncomando sbagliato\n");
-
+                printf("\nComando sbagliato\n");
         }
     } while (scelta != 0);
 
-
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
 
 void insertstudente() {
     studente s;
-    printf("\ninserisci il nome: ");
-    scanf("%s", s.nome);
-    printf("\ninserisci il cognome: ");
-    scanf("%s", s.cognome);
-    printf("\ninserisci la matricola: ");
+    printf("\nInserisci il nome: ");
+    scanf("%19s", s.nome);  
+    printf("Inserisci il cognome: ");
+    scanf("%19s", s.cognome);  
+    printf("Inserisci la matricola: ");
     scanf("%d", &s.matricola);
+
     FILE *fp = fopen("studenti.bin", "ab");
-    int k = fwrite(&s, sizeof (studente), 1, fp);
+    if (fp == NULL) {
+        printf("\nErrore nell'apertura del file studenti.bin\n");
+        return;
+    }
+
+    int k = fwrite(&s, sizeof(studente), 1, fp);
     fclose(fp);
-    printf("\nstudenti inseriti: %d", k);
+    
+    printf("\nStudente inserito: %d\n", k);
 }
 
 void insertvoto() {
@@ -79,50 +75,111 @@ void insertvoto() {
     voto v;
     char nome[20];
     char cognome[20];
-    printf("\ninserisci il nome: ");
-    scanf("%s", nome);
-    printf("\ninserisci il cognome: ");
-    scanf("%s", cognome);
-    printf("\ninserisci disciplina: ");
-    scanf("%s", v.disciplina);
-    printf("\ninserisci la valutazione: ");
+    
+    printf("\nInserisci il nome: ");
+    scanf("%19s", nome);  // Safe input
+    printf("Inserisci il cognome: ");
+    scanf("%19s", cognome);  // Safe input
+    printf("Inserisci la disciplina: ");
+    scanf("%19s", v.disciplina);  // Safe input
+    printf("Inserisci la valutazione: ");
     scanf("%d", &v.val);
 
-
     FILE *fp = fopen("studenti.bin", "rb");
+    if (fp == NULL) {
+        printf("\nErrore nell'apertura del file studenti.bin\n");
+        return;
+    }
+
     int trovato = 0;
-    while (fread(&s, sizeof (studente), 1, fp) > 0 && trovato == 0) {
-        if (strcmp(s.nome, nome) == 0 && strcmp(s.cognome, cognome) == 0)
+    while (fread(&s, sizeof(studente), 1, fp) > 0 && trovato == 0) {
+        if (strcmp(s.nome, nome) == 0 && strcmp(s.cognome, cognome) == 0) {
             trovato = 1;
+        }
     }
     fclose(fp);
 
-    printf("\n la matricola: %d\n", s.matricola);
     if (trovato == 1) {
         v.matricola = s.matricola;
-        printf("\n setto la matricola  2\n");
+
         FILE *fp1 = fopen("voti.bin", "ab");
         if (fp1 == NULL) {
-
-            printf("Errore apertura file");
-            exit(0);
+            printf("\nErrore nell'apertura del file voti.bin\n");
+            return;
         }
 
-        fwrite(&v, sizeof (voto), 1, fp1);
+        fwrite(&v, sizeof(voto), 1, fp1);
         fclose(fp1);
 
+        printf("\nVoto inserito per lo studente con matricola %d\n", s.matricola);
     } else {
-        printf("\nlo studente %s %s non esiste\n", nome, cognome);
+        printf("\nLo studente %s %s non esiste\n", nome, cognome);
     }
-
 }
 
 void stampastudenti() {
     studente s;
     FILE *fp = fopen("studenti.bin", "rb");
+    if (fp == NULL) {
+        printf("\nErrore nell'apertura del file studenti.bin\n");
+        return;
+    }
 
-    while (fread(&s, sizeof (studente), 1, fp) > 0) {
-        printf("\n\t nome: %s\t cognome: %s \tmatricola: %d\n", s.nome, s.cognome, s.matricola);
+    printf("\nLista degli studenti:\n");
+    while (fread(&s, sizeof(studente), 1, fp) > 0) {
+        printf("\tNome: %s\tCognome: %s\tMatricola: %d\n", s.nome, s.cognome, s.matricola);
     }
     fclose(fp);
+}
+
+
+void calcolamedia() {
+    studente s;
+    voto v;
+    
+    FILE *fp = fopen("studenti.bin", "rb");
+    if (fp == NULL) {
+        printf("\nErrore nell'apertura del file studenti.bin\n");
+        return;
+    }
+
+    FILE *fp_voti = fopen("voti.bin", "rb");
+    if (fp_voti == NULL) {
+        printf("\nErrore nell'apertura del file voti.bin\n");
+        fclose(fp);  
+        return;
+    }
+
+    printf("\nMedia e voti per ogni studente:\n");
+    
+   
+    while (fread(&s, sizeof(studente), 1, fp) > 0) {
+        int somma_voti = 0;
+        int numero_voti = 0;
+
+        
+        rewind(fp_voti);
+        
+        printf("\nStudente: %s %s (Matricola: %d)\n", s.nome, s.cognome, s.matricola);
+        printf("Voti:\n");
+
+       
+        while (fread(&v, sizeof(voto), 1, fp_voti) > 0) {
+            if (v.matricola == s.matricola) {
+                printf("\tDisciplina: %s\tValutazione: %d\n", v.disciplina, v.val);
+                somma_voti += v.val;
+                numero_voti++;
+            }
+        }
+
+        if (numero_voti > 0) {
+            float media = (float) somma_voti / numero_voti;
+            printf("Media voti: %.2f\n", media);
+        } else {
+            printf("Nessun voto disponibile\n");
+        }
+    }
+
+    fclose(fp);
+    fclose(fp_voti);
 }
